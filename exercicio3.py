58ace6c716c5f022f4f9dc26fc5c9398bc266572
Eduardo Guimarães <edu.guisantos@outlook.com>
2023-08-30 03:23:30 -0300

    Adicionando comentários ao exercício 3

--- exercicio3.py
+++ exercicio3.py
@@ -12,17 +12,20 @@ def limpar():
     else:
         system('clear')
 
+# Função para perguntar o peso do cachorro e valida-la
 def cachorro_peso():
     global limpar_tela
     global valor_total
 
     peso = ''
     valor_base = 0
-    valido = False
+    valido = False # Caso essa varável continue falsa, ou seja, o usuário insira um valor inválido,
+                   # o mesmo permanecerá preso no loop até que insira um valor válido.
 
     try:
         peso = int(input("\nEntre com o peso do cachorro: "))
 
+        # O valor base do peso será somado à variável "valor_total"
         if peso < 3:
             valor_base = 40
             valor_total =+ valor_base
@@ -52,6 +55,7 @@ def cachorro_peso():
 
     return peso, valor_base, valido
 
+# Função para perguntar o tamanho do peso do cachorro e valida-la.
 def cachorro_pelo():
     global valor_total
 
@@ -62,6 +66,8 @@ def cachorro_pelo():
     try:
         pelo = input("\n- Entre com o pelo do cachorro -\nc - Pelo Curto\nm - Pelo Médio\nl - Pelo Longo\n\nR: ")
 
+        # A variável global "valor_total" multiplicará ela mesmo pela variável "multi", na qual
+        # armazena o fator da multiplicação com base no tamanho do pelo do cachorro.
         if pelo == 'c':
             multi = 1
             valido = True
@@ -85,12 +91,15 @@ def cachorro_pelo():
 
     return pelo, multi, valido
 
+# Função para perguntar se o usuário deseja serviços adicionais e valida-la.
 def cachorro_extra():
     global valor_total
     global valor_adicionais
-    global servicos
+    global servicos # Chama a lista onde estão armazenados os serviços escolhidos, evitando que
+                    # o usuário selecione o mesmo serviço mais de uma vez.
 
     valido = False
+    extra = 0
 
     print("\n- Deseja adicionar mais algum serviço? -\n")
     print("1 - Corte de unhas - R$ 10,00")
@@ -104,6 +113,8 @@ def cachorro_extra():
         if extra == 1:
             nome = "Cortes de unha"
 
+            # Caso o serviço selecionado ainda não tiver sido escolhido, ou seja, não estiver na lista "servicos",
+            # o serviço estará disponível para ser selecionado.
             if nome not in servicos:
                 servicos.append(nome)
                 valor_total += 10
@@ -155,11 +166,21 @@ def cachorro_extra():
 
     return extra, valido
 
+# Armazena o valor total, incluindo o preço base para o tamanho do cachorro,
+# o fator de multiplicação para o tamanho do pelo e o serviço adicional.
 valor_total = 0
+
+# Armazena somente o preço dos serviços adicionais.
 valor_adicionais = 0
+
+# Lista que armazenará os serviços adicionais já selecionados, evitando que
+# o usuário selecione-o mais de uma vez.
 servicos = []
+
+# Variável que decidirá se a tela deve ou não ser limpada.
 limpar_tela = True
 
+# Programa principal.
 while True:
     if limpar_tela == True:
         limpar()
@@ -167,15 +188,20 @@ while True:
     
     peso, valor_base, valido = cachorro_peso()
 
+    # Caso o peso do cachorro seja válido, prosseguirá para próxima etapa.
     if valido == True:
         while True:
             pelo, multi, valido = cachorro_pelo()    
 
+            # Caso o pelo do cachorro seja válido, prosseguirá para próxima etapa.
             if valido == True:
                 while True:
                     extra, valido = cachorro_extra()
 
+                    # Caso o serviço adicional seja válido, irá perguntar se deseja adicionar mais algum.
                     if valido == True:
+
+                        # Caso o usuário não deseje mais nada, o total a pagar e o calculo da conta serão exibidos.
                         if extra == 0:
                             print("\nTotal a pagar(R$): {:.2f} (peso: {} * pelo: {} + adicional(is): {})".format(valor_total, valor_base, multi, valor_adicionais))
                             break
