6e930c9fcd2b9d1f6f42e8e7e54c3f4ecc394286
Eduardo Guimarães <edu.guisantos@outlook.com>
2023-08-31 07:03:37 -0300

    Adicionando função de remover colaborador

--- exercicio4.py
+++ exercicio4.py
@@ -17,28 +17,44 @@ def realce(titulo):
     print("{}{}{}".format('| ', titulo, ' |'))
     print("{}{}{}".format('+', '-' * (len(titulo) + 2), '+'))
 
-def separador(titulo = ''):
+def separador(titulo = '', limite = 0):
     global separador_tamanho
 
     if titulo:
         titulo = ("{}{}{}".format(' ', titulo, ' '))
         titulo_original = titulo
 
-        i = 0
-        while len(titulo) < separador_tamanho:
-            titulo = ("{}{}{}".format('-' * i, titulo_original, '-' * i))
-            i += 1
+        if not limite:
+            i = 0
+            while len(titulo) < separador_tamanho:
+                titulo = ("{}{}{}".format('-' * i, titulo_original, '-' * i))
+                i += 1
 
-        if len(titulo) == (separador_tamanho + 1):
-            titulo = titulo[:-1]
+            if len(titulo) == (separador_tamanho + 1):
+                titulo = titulo[:-1]
+
+        else:
+            i = 0
+            while len(titulo) < limite:
+                titulo = ("{}{}{}".format('-' * i, titulo, '-' * i))
+                i += 1
+
+            if len(titulo) == (limite + 1):
+                titulo = titulo[:-1]
 
         print(titulo)
     else:
-        print('-' * separador_tamanho)
+        if not limite:
+            print('-' * separador_tamanho)
+        else:
+            print('-' * limite)
 
 def cadastrar_colaborador(id):
     global lista_colaboradores
+    global limpar_tela
     global separador
+
+    limpar_tela = False
 
     separador("MENU CADASTRAR COLABORADOR")
     nome = input("Por favor, entre com o nome: ").title()
@@ -48,6 +64,9 @@ def cadastrar_colaborador(id):
     dicionario = {"id": id, "nome": nome, "setor": setor, "salario": salario}
 
     lista_colaboradores.append(dicionario)
+
+    print("\nColaborador cadastrado com sucesso!")
+    sleep(3)
 
 def consultar_colaborador():
     global limpar_tela
@@ -67,9 +86,11 @@ def consultar_colaborador():
             limpar_tela = False
 
             for colaborador in lista_colaboradores:
+                print("")
                 for key, value in colaborador.items():
                     print("{}: {}".format(key.title(), value))
 
+            separador(limite = 25)
             continue
         
         elif resposta == 2: # consultar por id
@@ -83,7 +104,12 @@ def consultar_colaborador():
             sleep(3)
 
 def remover_colaborador():
-    resposta = int(input(""))
+    remover = int(input("Digite o ID do colaborador a ser removido: "))
+    for colaborador in lista_colaboradores:
+        for key, value in colaborador.items():
+            print(key, value)
+            sleep(4234)
+            
 
 limpar_tela = True
 
@@ -96,36 +122,37 @@ while True:
     if limpar_tela == True:
         limpar()
         realce("Bem-vindo ao controle de Colaboradores do Eduardo Guimarães dos Santos")
-        separador("MENU PRINCIPAL")
-        print("Escolha a opção desejada:\n")
-        print("1 - Cadastrar Colaborador")
-        print("2 - Consultar Colaborador(res)")
-        print("3 - Remover Colaborador")
-        print("4 - Sair")
-        separador()
+    separador("MENU PRINCIPAL")
+    print("Escolha a opção desejada:\n")
+    print("1 - Cadastrar Colaborador")
+    print("2 - Consultar Colaborador(res)")
+    print("3 - Remover Colaborador")
+    print("4 - Sair")
+    separador()
 
-        try:
-            option = int(input("R: "))
+    try:
+        option = int(input("R: "))
 
-            if option == 1:
-                cadastrar_colaborador(id_global + 1)
+        if option == 1:
+            id_global += 1
+            cadastrar_colaborador(id_global)
 
-            elif option == 2:
-                consultar_colaborador()
+        elif option == 2:
+            consultar_colaborador()
 
-            elif option == 3:
-                remover_colaborador()
+        elif option == 3:
+            remover_colaborador()
 
-            elif option == 4:
-                print("\nAté mais!")
-                sleep(3)
-                break
-
-            else:
-                print("\nOpção inválida! Por favor, tente novamente.")
-                sleep(3)
-                continue
-        
-        except ValueError:
-            print("Opção inválida! Por favor, tente novamente.")
+        elif option == 4:
+            print("\nAté mais!")
             sleep(3)
+            break
+
+        else:
+            print("\nOpção inválida! Por favor, tente novamente.")
+            sleep(3)
+            continue
+    
+    except ValueError:
+        print("Opção inválida! Por favor, tente novamente.")
+        sleep(3)
