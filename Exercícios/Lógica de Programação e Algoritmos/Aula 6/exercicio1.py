1da331f1b6e0cc3b2580bb4efadb7e93e2c12728
Eduardo Guimarães <edu.guisantos@outlook.com>
2023-09-13 06:24:24 -0300

    Adicionando exercicio 2 da aula 6

--- Exercícios/Lógica de Programação e Algoritmos/Aula 6/exercicio1.py
+++ Exercícios/Lógica de Programação e Algoritmos/Aula 6/exercicio1.py
@@ -1,10 +1,19 @@
 palavras = ('sonic', 'eduardo', 'mario', 'andressa', 'linux',
             'python', 'backend', 'michael', 'beatles', 'madrugada')
 
+# Minha forma
 vogais = ('a', 'e', 'i', 'o', 'u')
 for palavra in palavras:
     print("\n{} = ".format(palavra), end=' ')
     for vogal in vogais:
         if vogal in palavra:
-            print("{}".format(vogal), end=' ')
+            print("{}".format(vogal.upper()), end=' ')
+print()
+
+# Forma do professor
+for palavra in palavras:
+    print("\n{} =".format(palavra), end=' ')
+    for letra in palavra:
+        if letra in "aeiou":
+            print(letra.upper(), end=' ')
 print()
